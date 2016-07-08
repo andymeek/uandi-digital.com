525f369c88fe94c1d8364aa40cfcba8bd7131a8a
Andy Meek <andy.meek@oakton.com.au>
2016-07-08 14:53:26 +1000

    Updates to services copy and recent builds styling

--- app/config.js
+++ app/config.js
@@ -1,21 +1,18 @@
-/*jslint node: true */
-'use strict';
+"use strict";
 
-var oConfig = {};
+let oConfig = {
+    COMPANY_NAME: 'U&amp;i Digital',
+    CONTACT_EMAIL: 'hello@uandi-digital.com',
+    TWITTER: 'http://twitter.com/andymeek',
+    FACEBOOK: 'https://www.facebook.com/U.and.i.Digital',
+    GITHUB: 'http://www.github.com/andymeek',
+    LINKEDIN: 'http://www.linkedin.com/profile/view?id=86256897',
 
-oConfig = {
-	COMPANY_NAME: 'U&amp;i Digital',
-	CONTACT_EMAIL: 'hello@uandi-digital.com',
-	TWITTER: 'http://twitter.com/andymeek',
-	FACEBOOK: 'https://www.facebook.com/U.and.i.Digital',
-	GITHUB: 'http://www.github.com/andymeek',
-	LINKEDIN: 'http://www.linkedin.com/profile/view?id=86256897',
+    pageTitle: '',
+    bodyClass: '',
+    pageDescription: '',
 
-	pageTitle: '',
-	bodyClass: '',
-	pageDescription: '',
-
-	isDevEnv: false
+    isDevEnv: false
 };
 
 exports.config = oConfig;
